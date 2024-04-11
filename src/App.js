f899058134f82d89f9e363fbe43bccc5e4e9bbce
James Erpenbeck <jimerp821@gmail.com>
2024-04-11 14:49:51 -0400

    very minimal grocery list app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+const initialList = [
+  { name: "Potatoes", quantity: 3, price: 1, id: crypto.randomUUID() },
+  { name: "Toilet Paper", quantity: 2, price: 8.99, id: crypto.randomUUID() },
+  { name: "Blueberries", quantity: 1, price: 4, id: crypto.randomUUID() },
+];
+
+export default function App() {
+  const [items, setItems] = useState(initialList);
+
+  function handleDeleteItem(id) {
+    setItems(() => items.filter((item) => item.id !== id));
+  }
+
+  function handleAddItem(item) {
+    setItems([...items, item]);
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  const total = items.reduce((acc, current) => acc + current.price, 0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GroceryList onDelete={handleDeleteItem} items={items} />
+      {items.length > 0 && (
+        <>
+          <span className="list-item">Total: {`$${total}`}</span>
+          <Button onClick={handleClearList}>Clear List</Button>
+        </>
+      )}
+      <AddItemForm onAddItem={handleAddItem} />
     </div>
   );
 }
 
-export default App;
+function AddItemForm({ onAddItem }) {
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [price, setPrice] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name || !quantity || !price) return;
+
+    const id = crypto.randomUUID();
+    const newItem = {
+      name: name[0].toUpperCase() + name.slice(1),
+      quantity,
+      price,
+      id,
+    };
+
+    onAddItem(newItem);
+    setName("");
+    setQuantity("");
+    setPrice("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="list-item">
+        <p>Add Item:</p>
+        <input
+          type="text"
+          placeholder="Item name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        />
+        <input
+          type="text"
+          placeholder="Price"
+          value={price}
+          onChange={(e) => setPrice(Number(e.target.value))}
+        />
+        <Button>Submit</Button>
+      </div>
+    </form>
+  );
+}
+function GroceryList({ items, onDelete }) {
+  return (
+    <div>
+      {items.map((item) => (
+        <ListItem onDelete={onDelete} item={item} key={item.name} />
+      ))}
+    </div>
+  );
+}
+
+function ListItem({ item, onDelete }) {
+  return (
+    <div className="list-item">
+      {`${item.name} | Quantity: ${item.quantity} | Price: ${item.price}`}
+      <Button onClick={() => onDelete(item.id)}>Delete</Button>
+    </div>
+  );
+}
+
+function Button({ children, onClick }) {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
+}
